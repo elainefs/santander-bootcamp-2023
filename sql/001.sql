c7296cf5c8eb000968fc3c1db0db515e584a6849
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-10-15 01:48:21 -0300

    feat: add sql scripts

---
+++ sql/001.sql
@@ -0,0 +1,30 @@
+-- Cria o banco de dados --
+CREATE DATABASE viagens
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+-- Define o banco a ser usado --
+USE viagens;
+
+-- Cria as tabelas --
+CREATE TABLE usuarios (
+  id INT,
+  nome VARCHAR(255) NOT NULL COMMENT 'Nome do usuário',
+  email VARCHAR(255) NOT NULL UNIQUE COMMENT 'Endereço de e-mail do usuário',
+  data_nascimento DATE NOT NULL COMMENT 'Data de nascimento do usuário',
+  endereco VARCHAR(50) NOT NULL COMMENT 'Endereço do Cliente'
+);
+
+CREATE TABLE viagens.destinos (
+  id INT,
+  nome VARCHAR(255) NOT NULL UNIQUE COMMENT 'Nome do destino',
+  descricao VARCHAR(255) NOT NULL COMMENT 'Descrição do destino'
+);
+
+CREATE TABLE viagens.reservas (
+  id INT COMMENT 'Identificador único da reserva',
+  id_usuario INT COMMENT 'Referência ao ID do usuário que fez a reserva',
+  id_destino INT COMMENT 'Referência ao ID do destino da reserva',
+  data DATE COMMENT 'Data da reserva',
+  status VARCHAR(255) DEFAULT 'pendente' COMMENT 'Status da reserva (confirmada, pendente, cancelada, etc.)'
+);
