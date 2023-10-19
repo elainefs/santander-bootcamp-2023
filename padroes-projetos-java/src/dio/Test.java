537b07f5372bccbcad49c48db7e079f690d63b3d
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-10-19 14:57:05 -0300

    feat: add test about design pattern

---
+++ padroes-projetos-java/src/dio/Test.java
@@ -0,0 +1,57 @@
+package dio;
+
+import dio.facade.Facade;
+import dio.singleton.SingletonEager;
+import dio.singleton.SingletonLazy;
+import dio.singleton.SingletonLazyHolder;
+import dio.strategy.Comportamento;
+import dio.strategy.ComportamentoAgressivo;
+import dio.strategy.ComportamentoDefensivo;
+import dio.strategy.ComportamentoNormal;
+import dio.strategy.Robo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		// Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		
+		// Strategy
+		
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		// Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Elaine", "1094756");
+	}
+
+}
